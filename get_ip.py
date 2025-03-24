6a3a4ef4da74dc179ed527e24c82336d72d4f031
Hernán <hernan@hego.com.ar>
2025-03-24 20:46:21 -0300

    Update get_ip.py

--- get_ip.py
+++ get_ip.py
@@ -1,1 +1,33 @@
+import requests
+import socket
 
+def get_telegram_ip():
+    try:
+        ip = socket.gethostbyname('api.telegram.org')
+        return ip
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+if __name__ == "__main__":
+    current_ip = get_telegram_ip()
+    if current_ip:
+        with open("last_ip.txt", "r") as f:
+            last_ip = f.read().strip()
+        
+        if current_ip != last_ip:
+            with open("last_ip.txt", "w") as f:
+                f.write(current_ip)
+            
+            # Actualiza timbre.php
+            with open("timbre.php", "r") as f:
+                php_code = f.read()
+            
+            new_php_code = php_code.replace(last_ip, current_ip)
+            
+            with open("timbre.php", "w") as f:
+                f.write(new_php_code)
+            
+            print("IP actualizada correctamente.")
+        else:
+            print("La IP no ha cambiado.")
